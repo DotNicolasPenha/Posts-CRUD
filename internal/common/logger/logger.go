2e24ebabbbfa55c41304f5f9072a2e6c497229e5
DotNicolasPenha <nicolaspenha45@gmail.com>
2026-02-18 18:12:37 -0300

    feat: in logger: GroupError

--- internal/common/logger/logger.go
+++ internal/common/logger/logger.go
@@ -3,11 +3,17 @@ package logger
 import "fmt"
 
 func Info(text string) {
-	fmt.Printf("[info] %s", text)
+	fmt.Printf("[info] %s \n", text)
 }
-func Error(text string) {
-	fmt.Printf("[error] %s", text)
+func Error(err error) {
+	fmt.Printf("[error] %s \n", err.Error())
+}
+func GroupError(errs []error) {
+	fmt.Println("Errors:")
+	for i, e := range errs {
+		fmt.Printf("[%d] %s", i, e.Error())
+	}
 }
 func Fatal(text string) {
-	fmt.Printf("[fatal] %s", text)
+	fmt.Printf("[fatal] %s \n", text)
 }
