8ff4d383008dbbca3d79c33daa28c999cce3e575
DotNicolasPenha <nicolaspenha45@gmail.com>
2026-02-21 17:10:06 -0300

    feat: setup table users

--- database/setup.go
+++ database/setup.go
@@ -17,6 +17,9 @@ func SetupTables(conn *pgxpool.Pool) error {
 	if err := setupTablePost(conn, ctx); err != nil {
 		return err
 	}
+	if err := setupTableUser(conn, ctx); err != nil {
+		return err
+	}
 	return nil
 }
 func setupExtensions(conn *pgxpool.Pool, ctx context.Context) error {
@@ -26,6 +29,21 @@ func setupExtensions(conn *pgxpool.Pool, ctx context.Context) error {
 	_, err := conn.Exec(ctx, query)
 	if err != nil {
 		return fmt.Errorf("Error to add extension pgcrypto: %s", err)
+	}
+	return nil
+}
+func setupTableUser(conn *pgxpool.Pool, ctx context.Context) error {
+	query := ` 
+	CREATE TABLE IF NOT EXISTS users (
+	  ID UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+		username TEXT NOT NULL,
+		bio TEXT NOT NULL,
+		created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+	)
+	`
+	_, err := conn.Exec(ctx, query)
+	if err != nil {
+		return fmt.Errorf("Error to create the user table: %s", err)
 	}
 	return nil
 }
